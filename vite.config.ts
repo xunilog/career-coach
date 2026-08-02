95c7f6c5026b3af790d01dae2f75bd8e553c5cd7
Yann Combarnous <yanncombarnous@Yanns-MacBook-Pro.local>
2026-08-02 18:19:25 +0200

    fix builds

--- vite.config.ts
+++ vite.config.ts
@@ -21,8 +21,7 @@ export default defineConfig({
   envPrefix: ["VITE_", "TAURI_ENV_*"],
 
   build: {
-    target: process.env.TAURI_ENV_PLATFORM === "windows" ? "chrome105" : "safari13",
-    minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
+    minify: !process.env.TAURI_ENV_DEBUG ? "oxc" : false,
     sourcemap: !!process.env.TAURI_ENV_DEBUG,
   },
 });
